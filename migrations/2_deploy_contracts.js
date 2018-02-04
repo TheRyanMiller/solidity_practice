238a97a1cfda9c0dfc503d5dc9d4c84013838c79
Ryan <rmiller07@gmail.com>
2018-02-04 01:59:31 -0500

    add app and ignore node_modules

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  //deployer.deploy(ConvertLib);
+  //deployer.link(ConvertLib, MetaCoin);
+  //deployer.deploy(MetaCoin);
+  deployer.deploy(Voting, 1000, web3.toWei('0.1', 'ether'), ['Ryan Miller', 'Donald Trump', 'Hillary Clinton']);
 };
